26245ec7f0897bb2079923aeecd91e7f4c913709
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-30 18:12:37 +0200

    feat: created header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Header.scss";
+import { useSelector } from "react-redux";
+import { MdPersonOutline, MdOutlineLogout } from "react-icons/md";
+import { updateAuthStoreStateLogout } from "../../features/authentication/updateAuthState";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const authState = useSelector((state) => state.auth);
+  const isLoggedIn = authState.isLoggedIn;
+  const { name, role } = authState.userInfo;
+  const isAdmin = role == "admin";
+
+  // handlers
+  const handleLogout = () => {
+    updateAuthStoreStateLogout();
+    navigate("/");
+  };
+
   return (
-    <div>Header</div>
-  )
+    <div className="Header">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <NavLink className="nav-link text-white" to="/">
+            <img src="../../../img/img logo.png" alt="imagen-logo"  className="logo" />
+          </NavLink>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              {isLoggedIn && !isAdmin && (
+                <NavLink className="nav-link" to="/events">
+                  Events
+                </NavLink>
+              )}
+              {isAdmin && (
+                <NavLink className="nav-link" to="/admin">
+                  Admin
+                </NavLink>
+              )}
+            </Nav>
+            {!isLoggedIn && (
+              <Nav>
+                <NavLink className="nav-link" to="/register">
+                  Registrarse
+                </NavLink>
+              </Nav>
+            )}
+            {isLoggedIn && (
+              <Nav>
+                <NavDropdown title={name} id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="/profile">
+                    <MdPersonOutline /> Perfil
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    <MdOutlineLogout /> Cerrar sesión
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
