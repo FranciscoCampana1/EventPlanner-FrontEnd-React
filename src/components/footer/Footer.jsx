2143341921c55f737d5c3a67bea5954288f0c51e
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-30 18:12:17 +0200

    feaet: created footer initial

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import './Footer.scss'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="contenedor-footer">
+    <div className="contenedor-redes">
+      <a href="" target="_blank" class="m-3 link">
+        <img
+          src="../../../img/intagram.jpg"
+          alt="imagenInstagram"
+          width="30"
+        />{" "}
+        Instagram
+      </a>
+      </div>
+      <div>
+      <a href="" target="_blank" class="m-3 link">
+        <img
+          src="../../../img/facebook.jpg"
+          alt="imagenFacebook"
+          width="28px"
+        />{" "}
+        Facebook
+      </a>
+    </div>
+  </div>
   )
 }
