018519682e0061e8250504b37e86bdfe208b2c3b
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-31 13:56:08 +0200

    feat: craete only view for admin

---
+++ src/_services/userService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { global } from "../_global/global";
+
+const userService = {};
+
+userService.getAllUsers = async (token, page = 1) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return (await axios.get(global.BASE_URL + `/api/users/get-all?page=${page}`, config))
+    .data;
+};
+
+export default userService;
--- src/containers/admin/Admin.jsx
+++ src/containers/admin/Admin.jsx
@@ -1,7 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import userService from "../../_services/userService";
+import { DataListTable } from "../../components";
+import { dateFormat } from "../../_util/date";
+import "./Admin.scss";
+
 
 export default function Admin() {
+    //hooks
+    const navigate = useNavigate();
+    const authState = useSelector((state) => state.auth);
+    const [users, setUsers] = useState([]);
+    const [usersPage, setUsersPage] = useState(1);
+    const [usersPages, setUsersPages] = useState(0);
+    const [usersCount, setUsersCount] = useState(0);
+    const {name} = authState.userInfo
+    const isAdmin = authState.userInfo.role == "admin";
+  
+    useEffect(() => {
+      if (isAdmin) {
+        getAllUsers(authState.userToken, usersPage);
+      } else {
+        navigate("/");
+      }
+    }, []);
+
+    const handleUsersList = (e) => {
+      const { page, dataId } = e.currentTarget.dataset;
+      handleUsersListPagination(page);
+      handleSingleUser(dataId);
+    };
+  
+    const handleUsersListPagination = (page) => {
+      switch (page) {
+        case "next":
+          return setUsersPage((page) => page + 1);
+        case "prev":
+          return setUsersPage((page) => page - 1);
+        case "first":
+          return setUsersPage(1);
+        case "last":
+          return setUsersPage(usersPages);
+      }
+    };
+  
+    const handleSingleUser = (dataId) => {
+      console.log(dataId);
+    };
+  
+    const getAllUsers = async (token, page) => {
+      try {
+        const response = await userService.getAllUsers(token, page);
+        setUsers(response.results);
+        setUsersCount(response.info.total_results);
+        setUsersPages(response.info.total_pages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+
+    const newUsers = (users) =>
+    users.map((user) => {
+       user.createdAt = dateFormat(user.createdAt);
+       user.updatedAt = dateFormat(user.updatedAt);
+       return user;
+    });
   return (
-    <div>Admin</div>
+    <div className= "contenedor-admin">
+      {isAdmin  && (
+          <>
+            <h1 style={{ textAlign: "center"}}>
+              Bienvenido {name}
+            </h1>
+            <DataListTable
+              data={newUsers(users)}
+              title="Users"
+              count={usersCount}
+              headers={["ID", "Nombre", "Apellidos", "Email", "Telefono", "Regitrado desde", "Última modificación" ]}
+              attributes={[
+                "id",
+                "name",
+                "surname",
+                "email",
+                "phone",
+                "createdAt",
+                "updatedAt"
+              ]}
+              pagination={{
+                page: usersPage,
+                totalPages: usersPages,
+                count: usersCount,
+              }}
+              onChange={handleUsersList}
+            />
+          </>
+        )}
+    </div>
   )
 }
