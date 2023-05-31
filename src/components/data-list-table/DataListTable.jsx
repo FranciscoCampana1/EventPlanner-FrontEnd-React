d87cc895760991280d65c1a673a58d266883687d
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-31 13:58:22 +0200

    fet: create component DataListTable

---
+++ src/components/data-list-table/DataListTable.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { TablePagination } from "../../components";
+import { Table } from "react-bootstrap";
+
+export default function DataListTable({
+  data,
+  title = "Data",
+  headers,
+  attributes,
+  onChange,
+  className,
+  pagination = null,
+}) {
+  return (
+    <div className={className}>
+      <Table bgcolor="white">
+        <thead>
+          <tr colSpan={headers.length}>
+            <th>
+              <div className="tableTitle"> {title}</div>
+            </th>
+          </tr>
+          <tr>
+            {headers.map((th, index) => (
+              <th key={index}>{th}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((d) => (
+            <tr scope="row" data-data-id={d.id} onClick={onChange} key={d.id}>
+              {attributes.map((attr, index) => (
+                <td data-label={headers[index]} key={index}>
+                  {d[attr] ? d[attr] : "No definido"}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+
+        {pagination && (
+          <tfoot>
+            <tr>
+              <td colSpan={headers.length}>
+                <TablePagination
+                  page={pagination.page}
+                  totalPages={pagination.totalPages}
+                  count={pagination.count}
+                  limit={data.length}
+                  onChange={onChange}
+                />
+              </td>
+            </tr>
+          </tfoot>
+        )}
+      </Table>
+    </div>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Header from "./header";
 import UsersList from "./users-list/UsersList.jsx";
 import TablePagination from "./table-pagination/";
+import DataListTable from "./data-list-table/DataListTable";
 import Footer from "./footer/Footer";
 
-export { Header, UsersList, TablePagination, Footer };
+export { Header, UsersList, TablePagination,DataListTable , Footer };
