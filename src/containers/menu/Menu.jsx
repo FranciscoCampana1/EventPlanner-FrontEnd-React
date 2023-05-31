e78a5580d8879abbe609f4ddae9b05d4549956b2
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-31 12:00:07 +0200

    feat: create menu of navigate in app

--- src/containers/menu/Menu.jsx
+++ src/containers/menu/Menu.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import "./Menu.scss";
 
 export default function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="menu">
+      <div className="contenedor">
+        <div className="contenedor-title">
+          <h2>Contactos</h2>
+        </div>
+        <div>
+          <img
+            src="../../../img/Sin título.png"
+            alt="ir a contactos"
+            className="imagen-contactos"
+          />
+        </div>
+      </div>
+      <div className="contenedor">
+        <div>
+            <h2>
+                Crear Evento
+            </h2>
+        </div>
+        <div>
+          <img
+            src="../../../img/crear evento2.png"
+            alt="imagen crear evento"
+            className="imagen-evento"
+          />
+        </div>
+      </div>
+      <div className="contenedor">
+        <div>
+          <h2>Próximos Eventos</h2>
+        </div>
+        <div>
+          <img
+            src="../../../img/proximos eventos.png"
+            alt="ver eventos"
+            className="imagen-ver-eventos"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
