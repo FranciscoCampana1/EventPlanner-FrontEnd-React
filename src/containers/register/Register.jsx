e4b5d630cf4940f079b2daf1e0677c38005e90bd
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-31 11:59:30 +0200

    feat: created register

--- src/containers/register/Register.jsx
+++ src/containers/register/Register.jsx
@@ -1,7 +1,92 @@
-import React from 'react'
+import React from "react";
+import './Register.scss'
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import authService from "../../_services/authService";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const [formValues, setFormValues] = useState({});
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value, 
+    });
+  };
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    registerUser(formValues);
+    navigate('/')
+  };
+
+  const registerUser = async (body) => {
+    try {
+      const response = await authService.registerUser(body);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
+    <>
+      <div className="contenedor-form">
+        <Form noValidate onSubmit={handleSubmit} className="form">
+          <Form.Group className="mb-3  rounded p-4 inputForm">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Nombre "
+              name="name"
+              value={formValues.name}
+              onChange={handleChange}
+            />
+            <Form.Label>Apellidos</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Apellidos "
+              name="surname"
+              value={formValues.surname}
+              onChange={handleChange}
+            />
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Email "
+              name="email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+            <Form.Label>Telefono</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Teléfono "
+              name="phone"
+              value={formValues.phone}
+              onChange={handleChange}
+            />
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          
+            <Button variant="primary" type="submit" className="button">
+            Crear perfil
+          </Button>
+          
+          
+        </Form>
+      </div>
+    </>
+  );
 }
