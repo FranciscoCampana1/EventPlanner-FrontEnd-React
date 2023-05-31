682ea02060ae7585088a7de51dc0b651bf12c103
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-31 14:00:20 +0200

    feat: create folder of view EventCreate

---
+++ src/containers/event-create/EventCreate.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventCreate() {
+  return (
+    <div>EventCreate</div>
+  )
+}
--- src/containers/index.js
+++ src/containers/index.js
@@ -3,9 +3,10 @@ import Events from "./events/";
 import Diary from "./diary";
 import Home from "./home/";
 import Menu from "./menu/";
+import EventCreate from "./event-create/EventCreate";
 import Register from "./register/";
 import UserProfile from "./user-profile/UserProfile";
 import PageNotFound from "./page-not-found/PageNotFound";
 
 
-export { Admin, Events, Diary, Menu , Home, Register, UserProfile, PageNotFound }
+export { Admin, Events, Diary, Menu ,EventCreate , Home, Register, UserProfile, PageNotFound }
