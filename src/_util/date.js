90db80c5c4bdbdce0d3f033f3828968bbdc3bfbd
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-31 13:57:07 +0200

    feat : add function date format

---
+++ src/_util/date.js
@@ -0,0 +1,13 @@
+export const dateFormat = (dateString) => {
+    const fullDate = new Date(dateString);
+ 
+    const options = {
+       weekday: "long",
+       year: "numeric",
+       month: "long",
+       day: "numeric",
+    };
+ 
+    return fullDate.toLocaleDateString("es-ES");
+ };
+ 
