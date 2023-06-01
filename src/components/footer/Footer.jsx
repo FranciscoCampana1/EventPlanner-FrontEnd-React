a71690b093176bf34d2c7fd22a916efb082d5ee9
Francisco Campana <campanafrancisco1@gmail.com>
2023-06-01 14:17:27 +0200

    fix : error class in label <a></a>

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="contenedor-footer">
     <div className="contenedor-redes">
-      <a href="" target="_blank" class="m-3 link">
+      <a href="" target="_blank" className="m-3 link">
         <img
           src="../../../img/intagram.jpg"
           alt="imagenInstagram"
@@ -15,7 +15,7 @@ export default function Footer() {
       </a>
       </div>
       <div>
-      <a href="" target="_blank" class="m-3 link">
+      <a href="" target="_blank" className="m-3 link">
         <img
           src="../../../img/facebook.jpg"
           alt="imagenFacebook"
