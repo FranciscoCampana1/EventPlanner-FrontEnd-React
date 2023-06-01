8a91a5f8247e9ef82505d7b582c41b79f141c377
Francisco Campana <campanafrancisco1@gmail.com>
2023-06-01 14:20:16 +0200

    feat: view the diary

--- src/containers/diary/Diary.jsx
+++ src/containers/diary/Diary.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import userService from "../../_services/userService";
+import { DataListTable } from "../../components";
+import './Diary.scss'
 
 export default function Diary() {
+  //hooks
+  const authState = useSelector((state) => state.auth);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getMyContacts(authState.userToken);
+  }, []);
+
+  const getMyContacts = async (token) => {
+    try {
+      const response = await userService.getMyContacts(token);
+      setUsers(response.Contacts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const newContact = (users) =>
+    users.map((user) => {
+      user.id = user.Contact.user_id;
+      user.name = user.Contact.User.name;
+      user.surname = user.Contact.User.surname;
+      user.email = user.Contact.User.email;
+      user.phone = user.Contact.User.phone;
+      return user;
+    });
+
   return (
-    <div>
-        <h1>DIARY</h1>
-    </div>
-  )
+    <>
+      <div className="contenedor-agenda">
+        <DataListTable
+          data={newContact(users)}
+          title="Contacts"
+          headers={["Nombre", "Apellidos", "Email", "Telefono"]}
+          attributes={["name", "surname", "email", "phone"]}
+          
+        />
+      </div>
+    </>
+  );
 }
