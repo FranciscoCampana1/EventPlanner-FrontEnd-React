03db4725b8c2967cbc48bdca87a853b7b5b039ae
Francisco Campana <campanafrancisco1@gmail.com>
2023-06-01 14:18:05 +0200

    feat:  view for create event

--- src/containers/event-create/EventCreate.jsx
+++ src/containers/event-create/EventCreate.jsx
@@ -1,7 +1,114 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import Form from "react-bootstrap/Form";
+import { FormGroup } from "react-bootstrap";
+import eventService from "../../_services/eventService";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import './EventCreate.scss'
 
 export default function EventCreate() {
+  const authState = useSelector((state) => state.auth);
+  const [formValues, setFormValues] = useState({});
+  const navigate = useNavigate();
+
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createEvent(authState.userToken ,formValues);
+    navigate("/events");
+  };
+
+  const createEvent = async (token, body) => {
+    try {
+      const response = await eventService.createEvent(token, body);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div>EventCreate</div>
-  )
+    <>
+      <div className="contenedor-form">
+        <Form noValidate onSubmit={handleSubmit} className="form">
+          <Form.Group className="mb-3  rounded p-4 inputForm">
+            <Form.Label>Título</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Título "
+              name="title"
+              value={formValues.title}
+              onChange={handleChange}
+            />
+            <Form.Label>Descripción</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Descripción del evento (opcional)"
+              name="description"
+              value={formValues.description}
+              onChange={handleChange}
+            />
+            <Form.Label>Día</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Día"
+              name="date"
+              value={formValues.date}
+              onChange={handleChange}
+            />
+            <Form.Label>Hora</Form.Label>
+            <FormGroup>
+              <Form.Check
+                type={"radio"}
+                name="time"
+                value="12:00:00"
+                onChange={handleChange}
+                label={`12:00`}
+              />
+              <Form.Check
+                type={"radio"}
+                name="time"
+                value="15:00:00"
+                onChange={handleChange}
+                label={`15:00`}
+              />
+              <Form.Check
+                type={"radio"}
+                name="time"
+                value="20:00:00"
+                onChange={handleChange}
+                label={`20:00`}
+              />
+              <Form.Check
+                type={"radio"}
+                name="time"
+                value="22:00:00"
+                onChange={handleChange}
+                label={`22:00`}
+              />
+              <Form.Check
+                type={"radio"}
+                name="time"
+                value="23:59:00"
+                onChange={handleChange}
+                label={`23:59`}
+              />
+            </FormGroup>
+          </Form.Group>
+          <Button variant="primary" type="submit" className="button">
+            Crear evento
+          </Button>
+        </Form>
+      </div>
+    </>
+  );
 }
