864ec1f59132e44fcce67b62999e9e6de1c6161d
Francisco Campana <campanafrancisco1@gmail.com>
2023-06-01 16:50:00 +0200

    feat: create service for delete and update events

--- src/_services/eventService.js
+++ src/_services/eventService.js
@@ -32,6 +32,51 @@ eventService.getEvents = async (token) => {
     return (await axios.post(global.BASE_URL + `/api/event/create`, body, config))
       .data;
   }
+
+
+  eventService.updateEvent = async (token, data, event) => {
+    const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+    }
+
+    const body = {
+      title: data.title,
+      description: data.description,
+      date: data.date,
+      time: data.time,
+    };
+  
+    return (await axios.put(global.BASE_URL + `/api/event/update-event/${event}`, body, config))
+      .data;
+  }
+
+
+  eventService.deleteEvent = async (token, event) => {
+    const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+    }
+
+    return (await axios.delete(global.BASE_URL + `/api/event/delete-event/${event}`, config))
+      .data;
+  }
+
+
+  eventService.deleteInvitation = async (token, event) => {
+    const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+    }
+  
+    return (await axios.post(global.BASE_URL + `/api/event/delete-invitation/${event}`, config))
+      .data;
+  }
+
+  
   
 
 
