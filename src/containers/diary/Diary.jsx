b62a1ee099de7b0826f5549d4feabc5842758b87
Francisco Campana <campanafrancisco1@gmail.com>
2023-06-02 14:18:29 +0200

    feat: create view the contacts in events for add guests

--- src/containers/diary/Diary.jsx
+++ src/containers/diary/Diary.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import userService from "../../_services/userService";
 import { DataListTable } from "../../components";
-import './Diary.scss'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Diary.scss";
 
 export default function Diary() {
   //hooks
   const authState = useSelector((state) => state.auth);
   const [users, setUsers] = useState([]);
+  const [buttons, setButtons] = useState(true);
+  const [formValues, setFormValues] = useState({});
+  const [addContact, setAddContact] = useState(false);
 
   useEffect(() => {
     getMyContacts(authState.userToken);
@@ -17,6 +22,34 @@ export default function Diary() {
     try {
       const response = await userService.getMyContacts(token);
       setUsers(response.Contacts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const handleAddContact = () => {
+    setAddContact(true);
+    setButtons(false);
+  };
+  
+  const handleNewContact = () => {
+    addNewContact(authState.userToken, formValues);
+  };
+  const handleHideForm = () => {
+    setAddContact(false);
+    setButtons(true);
+  };
+
+  const addNewContact = async (token, data) => {
+    try {
+      const response = await userService.addContact(token, data);
     } catch (error) {
       console.log(error);
     }
@@ -31,6 +64,7 @@ export default function Diary() {
       user.phone = user.Contact.User.phone;
       return user;
     });
+    
 
   return (
     <>
@@ -40,8 +74,49 @@ export default function Diary() {
           title="Contacts"
           headers={["Nombre", "Apellidos", "Email", "Telefono"]}
           attributes={["name", "surname", "email", "phone"]}
-          
         />
+        <div className="contenedor-form-add-contact">
+          {addContact && (
+            <div>
+              <h3>Agendar Contacto </h3>
+              <Form
+                noValidate
+                onSubmit={handleNewContact}
+                className="form-modificar-evento"
+              >
+                <Form.Group className="mb-3  rounded p-4 inputForm">
+                  <Form.Label>Nº Móvil</Form.Label>
+                  <Form.Control
+                    type="integer"
+                    placeholder="692447415 "
+                    name="phone"
+                    value={formValues.phone}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <div className="buttons">
+                  <Button variant="primary" type="submit" className="button">
+                    Agendar Contacto
+                  </Button>
+                  <Button
+                    variant="danger"
+                    className="button"
+                    onClick={handleHideForm}
+                  >
+                    Cancelar
+                  </Button>
+                </div>
+              </Form>
+            </div>
+          )}
+          {buttons && (
+            <div className="buttons">
+              <button className="buttonAñadir" onClick={handleAddContact}>
+                Añadir Contacto
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
